f3b8893ebb1eb046ad8fe325d62ab672f56548d0
IRotcivI <148015994+IRotcivI@users.noreply.github.com>
2024-12-05 22:22:32 +0100

    Page du commencement du quiz

--- quiz/src/trait/traitement.php
+++ quiz/src/trait/traitement.php
@@ -1,6 +1,18 @@
 <?php
 
+use App\Http\Controllers\ScoreController;
 
-if(array_key_exists("inscription",$_POST)){
+include '../model/score.php';
+include '../controller/ScoreController.php';
 
+if (array_key_exists("inscription", $_POST)) {
+
+}
+
+if (array_key_exists("connexion", $_POST)) {
+    $score = new Score([
+        "pseudo" => $_POST["pseudo"],
+    ]);
+    $controller = new ScoreController();
+    $controller->start($score);
 }
