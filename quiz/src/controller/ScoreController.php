d3f182bfd921c40c02de81f96b45c376dbd6d5b7
IRotcivI <148015994+IRotcivI@users.noreply.github.com>
2024-12-05 22:33:31 +0100

    Page du commencement du quiz 3

--- quiz/src/controller/ScoreController.php
+++ quiz/src/controller/ScoreController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers;
 
 use Bdd;
+use Score;
 
 include '../bdd/Bdd.php';
 
 class ScoreController
 {
-    public function start()
+    public function start(Score $score)
     {
         $bdd = new Bdd();
         $req = $bdd->getBdd()->prepare("INSERT INTO score (pseudo, score) VALUES(:pseudo, :score)");
         $req->execute(array(
-            'pseudo' => $_POST['pseudo'],
+            'pseudo' => $score->getPseudo(),
             'score' => 0,
         ));
+        $req = $bdd->getBdd()->prepare("SELECT * FROM score WHERE pseudo = :pseudo");
+        $req->execute(array(['pseudo' => $score->getPseudo()]));
+        $result = $req->fetch();
+        session_start();
+        $score->setId($result['id_score']);
+        $score->setscore($result['score']);
+        $_SESSION['score'] = $score;
         header('location:/quiz/vue/quizRaceForWater.php');
     }
 }
